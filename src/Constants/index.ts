3f38371c9e19e06249f1b236743b15f99d4c0758
selly361 <seikou615@gmail.com>
2024-05-26 11:56:02 +0100

    Add: `index.ts` to export the constants

---
+++ src/Constants/index.ts
@@ -0,0 +1,5 @@
+export { plans } from './plans';
+export { addOns } from './addOns';
+export { formSteps } from './formSteps';
+export { stageDescriptions } from './stageDescriptions';
+export { slideAnimation } from './animations';
