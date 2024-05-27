5c2e1614323648196cfb5f929b57cb2bc7405417
selly361 <seikou615@gmail.com>
2024-05-27 19:20:50 +0100

    Add: `PlanSelector` components styles

---
+++ src/Components/PlanSelector/PlanSelector.styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	gap: 32px;
+`
+
+
+const PlanContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+`
+
+
+
+export { Container, PlanContainer }
