f6e97ce225549da2dfe60ed74e62d54522df6be6
selly361 <seikou615@gmail.com>
2024-05-28 12:05:23 +0100

    Update: refactor `useFormState` hook using `react-hook-form` package

--- src/Hooks/useFormState/index.tsx
+++ src/Hooks/useFormState/index.tsx
@@ -1,19 +1,25 @@
 import { useState, useCallback } from 'react'
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form'
 import { FormData } from 'Types'
 import { addOns, plans } from 'Constants'
 
 const useFormState = () => {
 	const [currentStep, setCurrentStep] = useState<number>(1)
 	const [frequency, setFrequency] = useState<'monthly' | 'yearly'>('monthly')
-	const [formData, setFormData] = useState<FormData>({
-		personalInfo: { name: '', email: '', phoneNumber: '' },
-		selectedPlan: plans[frequency][0],
-		addOns: addOns[frequency]
+
+	const methods = useForm<FormData>({
+		defaultValues: {
+			personalInfo: { name: '', email: '', phoneNumber: '' },
+			selectedPlan: plans[frequency][0],
+			addOns: addOns[frequency]
+		}
 	})
 
 	const handleNextStep = useCallback(() => {
-		setCurrentStep((prevStep) => Math.min(prevStep + 1, 4))
-	}, [])
+		methods.handleSubmit(() => {
+			setCurrentStep((prevStep) => Math.min(prevStep + 1, 4))
+		})()
+	}, [methods])
 
 	const handlePreviousStep = useCallback(() => {
 		setCurrentStep((prevStep) => Math.max(prevStep - 1, 1))
@@ -21,22 +27,19 @@ const useFormState = () => {
 
 	const handleFormDataChange = useCallback(
 		(key: keyof FormData, value: any) => {
-			setFormData((prevData) => ({
-				...prevData,
-				[key]: value
-			}))
+			methods.setValue(key, value)
 		},
-		[]
+		[methods]
 	)
 
 	return {
 		currentStep,
 		frequency,
-		formData,
 		setFrequency,
 		handleNextStep,
 		handlePreviousStep,
-		handleFormDataChange
+		handleFormDataChange,
+		methods
 	}
 }
 
