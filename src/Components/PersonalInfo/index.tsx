65232e74809c2e14f783b34e8e349c629633af25
selly361 <seikou615@gmail.com>
2024-05-28 12:06:01 +0100

    Add: `PersonalInfo` component

---
+++ src/Components/PersonalInfo/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react'
+import { useFormContext, FieldError, FieldErrorsImpl, Merge } from 'react-hook-form'
+import { StyledForm, StyledFieldset, StyledLabelContainer, StyledLabel, ErrorText, StyledInput } from './PersonalInfo.styles'
+import { PersonalInfoData } from 'Types'
+
+interface IPersonalInfoProps {
+    handleFormDataChange: (data: PersonalInfoData) => void
+}
+
+type PersonalInfoErrors = {
+    personalInfo?: {
+        name?: FieldError | Merge<FieldError, FieldErrorsImpl<any>>,
+        email?: FieldError | Merge<FieldError, FieldErrorsImpl<any>>,
+        phoneNumber?: FieldError | Merge<FieldError, FieldErrorsImpl<any>>
+    }
+}
+
+function PersonalInfo({ handleFormDataChange }: IPersonalInfoProps) {
+    const { register, formState: { errors }, watch } = useFormContext<PersonalInfoErrors>()
+
+    const formData = watch("personalInfo")
+
+    useEffect(() => {
+        handleFormDataChange(formData)
+    }, [formData, handleFormDataChange])
+
+    return (
+        <StyledForm>
+            <StyledFieldset>
+                <StyledLabelContainer>
+                    <StyledLabel htmlFor="name">Name</StyledLabel>
+                    {errors.personalInfo?.name && <ErrorText>{errors.personalInfo.name.message}</ErrorText>}
+                </StyledLabelContainer>
+                <StyledInput id="name" {...register('personalInfo.name', { required: 'Name is required' })} />
+            </StyledFieldset>
+
+            <StyledFieldset>
+                <StyledLabelContainer>
+                    <StyledLabel htmlFor="email">Email Address</StyledLabel>
+                    {errors.personalInfo?.email && <ErrorText>{errors.personalInfo.email.message}</ErrorText>}
+                </StyledLabelContainer>
+                <StyledInput id="email" {...register('personalInfo.email', { required: 'Email is required' })} />
+            </StyledFieldset>
+
+            <StyledFieldset>
+                <StyledLabelContainer>
+                    <StyledLabel htmlFor="phoneNumber">Phone Number</StyledLabel>
+                    {errors.personalInfo?.phoneNumber && <ErrorText>{errors.personalInfo.phoneNumber.message}</ErrorText>}
+                </StyledLabelContainer>
+                <StyledInput id="phoneNumber" {...register('personalInfo.phoneNumber', { required: 'Phone number is required' })} />
+            </StyledFieldset>
+        </StyledForm>
+    )
+}
+
+export default PersonalInfo
