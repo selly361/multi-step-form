46b450e5729615538ba5f3b91bce4f8aeecd0bb1
selly361 <seikou615@gmail.com>
2024-05-29 18:27:52 +0100

    Add: `Buttons` components styles

---
+++ src/Components/Buttons/Styled.buttons.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+const StyledFieldset = styled.fieldset`
+	width: 100%;
+	display: grid;
+	grid-template-columns: 63px 123px;
+	justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    bottom: 16px;
+`
+
+const NextStepButton = styled.button`
+	justify-self: end;
+	width: max-content;
+	height: 48px;
+	padding: 14px 24px 16px 25px;
+	background: #022959;
+	border-radius: 8px;
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 18px;
+	color: #ffffff;
+	grid-column: 2;
+	transition: 0.5s ease background;
+
+	@media (hover: hover) {
+		&:hover {
+			background: #164a8a;
+		}
+	}
+`
+
+const PrevStepButton = styled.button`
+	grid-column: 1;
+	background-color: transparent;
+	color: #9699aa;
+	transition: 0.5s ease color;
+    height: max-content;
+
+	@media (hover: hover) {
+		&:hover {
+			color: #022959;
+		}
+	}
+`
+
+export { StyledFieldset, NextStepButton, PrevStepButton }
